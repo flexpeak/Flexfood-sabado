90803e617c2b911dcb0f7c9712b5209f613857b8
ericoborgonove <borgonove@gmail.com>
2023-10-14 16:57:41 -0400

    error 404

--- frontend/src/pages/error404/Error404.jsx
+++ frontend/src/pages/error404/Error404.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
-import Card  from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import figuraErro from './erro.avif'
 
 function Error404() {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  )
+    return (
+        <Container style={{display: 'flex', justifyContent: 'center', alignItens: 'center', heigth: '100vh'}}>
+            <Card style={{ width: '400px' }}>
+                <Card.Img variant="top" src={figuraErro} />
+                <Card.Body style={{}}>
+                    <Card.Title style={{ width: '144pix', textAlign: 'center',display: 'flex' 
+                     , justifyContent: 'center', alignItens: 'center' }}>Error</Card.Title>
+                    <Card.Text style={{ width: '72pix', textAlign: 'center',display: 'flex' 
+                     , justifyContent: 'center', alignItens: 'center' }}>
+                        Page not Found
+                    </Card.Text>
+                    <Button variant="outline-danger" href='/'>Home</Button>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
 }
 export default Error404
